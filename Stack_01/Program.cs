cda76b92b75d3c1cb5728fc89aca4ecfb0d8da20
fakessoo <fakessoo83@gmail.com>
2021-08-08 08:58:24 -0700

    stack example added

---
+++ Stack_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creates and initializes a new Stack
+            Stack myStack = new Stack();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+
+            // Displays the properties and Values of the Stack.
+            Console.WriteLine("myStack");
+            Console.WriteLine("\tCount:     {0}", myStack.Count);
+            Console.Write("\tValues:");
+            PrintValues(myStack);
+        }
+
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+            {
+                Console.Write("     {0}", obj);
+            }
+            Console.WriteLine();
+        }
+    }
+}
