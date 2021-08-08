eb9cd7d7da899e8dcf6a3adef6fa5d85c0b6edcf
fakessoo <fakessoo83@gmail.com>
2021-08-08 09:39:25 -0700

    char class example added

---
+++ Char_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Char_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char chA = 'A';
+            char ch1 = '1';
+            string str = "test string";
+
+            Console.WriteLine(chA.CompareTo('B'));
+            Console.WriteLine(chA.Equals('A'));
+            Console.WriteLine(Char.GetNumericValue(ch1));
+            Console.WriteLine(Char.IsControl('\t'));
+            Console.WriteLine(Char.IsDigit(ch1));
+            Console.WriteLine(Char.IsLetter(','));
+            Console.WriteLine(Char.IsLower('u'));
+            Console.WriteLine(Char.IsNumber(ch1));
+            Console.WriteLine(Char.IsPunctuation('.'));
+            Console.WriteLine(Char.IsSeparator(str, 4));
+            Console.WriteLine(Char.IsSymbol('+'));
+            Console.WriteLine(Char.IsWhiteSpace(str, 4));
+            Console.WriteLine(Char.Parse("S"));
+            Console.WriteLine(Char.ToLower('M'));
+            Console.WriteLine('x'.ToString());
+
+        }
+    }
+}
