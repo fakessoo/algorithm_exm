b2ade3e34770fc77d058a4ccf240f049b16bc52c
fakessoo <fakessoo83@gmail.com>
2021-08-15 16:56:14 -0700

    937_Reorder_Data_in_Log_Files

---
+++ 26_Remove_Duplicates_from_Sorted_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _26_Remove_Duplicates_from_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{ 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            Console.WriteLine("Result : " + RemoveDuplicates(nums));
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            // 반복된 k를 삭제하는 경우 첫번째 k가 남는다
+            // return value 는 반복된 총 개수
+            // non-decreasing order라는것은 정렬은 되있으나 반복된 수가 존재하는 정렬을 가리킴
+            // compare check 
+            // 메모리 할당은 한번으로 끝나야 함 = Space Complexity : O(1)
+            // Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+            if (nums.Length == 0) 
+                return 0;
+
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            return i + 1;
+        }
+
+    }
+}
---
+++ 937_Reorder_Data_in_Log_Files/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _937_Reorder_Data_in_Log_Files
+{
+    class Program
+    {
+        public static void Main()
+        {
+#if false
+            string[] logs = new string[5];
+            logs[0] = "dig1 8 1 5 1";
+            logs[1] = "let1 art can";
+            logs[2] = "dig2 3 6";
+            logs[3] = "let2 own kit dig";
+            logs[4] = "let3 art zero";
+            ReorderLogFiles(logs);
+#else
+            
+            string[] logs = new string[] { "j je", "b fjt", "7 zbr", "m le", "o 33" };
+            ReorderLogFiles(logs);
+#endif
+        }
+
+        public static string[] ReorderLogFiles(string[] logs)
+        {
+            // 1. distinguish letter-logs and digits-logs
+            // 2. sorting letter-logs 
+            // 3. sorting digits-logs
+            // 4. compose both logs in a string array
+            // 5. return string array
+
+            string[] logs_arr = new string[logs.Length];
+
+            List<string> letters_list = new List<string>();
+            List<string> digits_list = new List<string>();
+
+            // distinguish letter-logs and digits-logs
+            for (int i = 0; i < logs.Length; i++)
+            {
+                string input_arr_str = logs[i].Split(' ', 2)[1];
+                if (!int.TryParse(input_arr_str[0].ToString(), out int input_num))
+                {
+                    // this is letter-log
+                    letters_list.Add(logs[i]);
+                }
+                else
+                {
+                    // this is digit-log
+                    digits_list.Add(logs[i]);
+                }
+            }
+
+            string[] letters_arr = letters_list.ToArray();
+            string[] digits_arr = digits_list.ToArray();
+
+            for (int i = 0; i < letters_arr.Length; i++)
+            {
+                if (letters_arr.Length < 2)
+                {
+                    logs_arr[i] = letters_arr[i];
+                }
+                else
+                {
+                    string[] splt_letter = letters_arr[i].Split(' ', 2);
+                    for (int j = i + 1; j < letters_arr.Length; j++)
+                    {
+                        string[] splt_letter2 = letters_arr[j].Split(' ', 2);
+                        if (string.Compare(splt_letter[1], splt_letter2[1]) < 0)
+                        {
+                            logs_arr[i] = string.Join(' ', splt_letter);
+                            splt_letter = letters_arr[i].Split(' ', 2);
+                            logs_arr[i + 1] = string.Join(' ', splt_letter2);
+                        }
+                        else
+                        {
+                            logs_arr[i] = string.Join(' ', splt_letter2);
+                            splt_letter = letters_arr[j].Split(' ', 2);
+                            logs_arr[i + 1] = string.Join(' ', splt_letter);
+                        }
+                    }
+                }
+            }
+
+            
+            for (int i = 0; i < digits_arr.Length; i++)
+            {
+                logs_arr[letters_arr.Length + i] = digits_arr[i];
+            }
+
+            return logs_arr;
+        }
+    }
+}
